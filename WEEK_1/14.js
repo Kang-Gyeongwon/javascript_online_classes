8b2f2c63ac11661e5ee4c23a27056c82ef21b415
Kang-Gyeongwon <rosie02644@gmail.com>
2023-05-23 15:51:07 +0900

    chore: 조건문의 falsy, truthy

---
+++ WEEK_1/14.js
@@ -0,0 +1,37 @@
+// falsy한 값, truthy한 값
+if (0) {    // 0은 number 타입 이 타입도 true 또는 false로 사용할 수 있다 
+            // 이러한 값들을 판단하는게 truthy falsy
+  // main logic
+  console.log("hello")
+}
+
+if ("") {   
+  // main logic
+  console.log("hello")
+}
+
+if (null) {   
+  // main logic
+  console.log("hello")
+}
+
+if (undefined) {   
+  // main logic
+  console.log("hello")
+}
+
+if (NaN) {   
+  // main logic
+  console.log("hello")
+}
+
+if (false) {   
+  // main logic
+  console.log("hello")
+}
+
+if (true){
+  console.log("hello")    // 이 조건문만 실행됨
+}
+
+// 모두 false한 값
