56a0ecb931780a3c9995357cdebadd2e950b818e
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-13 20:36:03 +0900

    feat: 게임 시작하기!

--- task/task01.js
+++ task/task01.js
@@ -1,27 +1,15 @@
-// 컴퓨터 임의 생산 숫자
-let computerNum = randomNum();
-
 // 사용자 입력 횟수
-let conut = 0;
-
-// 사용자로부터 입력받기
-const inputNum = () => {
-  const guest = prompt("세개의 숫자를 입력하시요!");
-
-  // 유효성 검사하기
-  if (isTrueNum(guest)) {
-    conut++;
-  }
-};
+let count = 0;
 
 // 유효성 검사 함수
-const isTrueNum = (guest) => {
+const isTrueNum = (guess) => {
   // 3자리 숫자인지 정규식으로 확인하기
-  return /^\d{3}$/.test(guest);
+  return /^\d{3}$/.test(guess);
 };
 
+// 숫자 0 - 9까지 랜덤으로 만드는 함수
 const randomNum = () => {
-  let numbers = {};
+  let numbers = [];
   while (numbers.length < 3) {
     const randomNumber = Math.floor(Math.random() * 10);
 
@@ -31,5 +19,45 @@ const randomNum = () => {
   }
   return numbers.join("");
 };
+// 컴퓨터 임의 생성 숫자
+let computerNum = randomNum();
 
-console.log(computerNum);
+// 스트라이크와 볼 확인 함수
+const strikeOrBall = (guess) => {
+  let strikes = 0;
+  let balls = 0;
+
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === computerNum[i]) {
+      strikes++;
+    } else if (computerNum.includes(Number(guess[i]))) {
+      balls++;
+    }
+  }
+  return { B: balls, S: strikes };
+};
+
+
+// 게임 시작하기
+while (true) {
+  count++;
+
+  // 입력 받기
+  const guess = prompt(`${count}번째 시도!`);
+
+  // 유효성 검사하기
+  if (isTrueNum(guess)) {
+    // 스트라이크, 볼 확인
+    const result = strikeOrBall(guess);
+
+    console.log(`${result.B}B${result.S}S`);
+
+    // 게임 종료
+    if (result.S === 3) {
+      console.log(`게임을 종료합니다. ${count}번만에 맞히셨습니다.`);
+      break;
+    }
+  } else {
+    console.log("잘못된 입력입니다. 다시 시도해주세요.");
+  }
+}
