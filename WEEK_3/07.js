72ac31de7073e41ee8bcdbfd02d91b9e84e67252
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-15 15:02:28 +0900

    chore: null

--- WEEK_3/07.js
+++ WEEK_3/07.js
@@ -1,11 +1,23 @@
 // undefined
 let a;
-console.log(a);  // undefined
+console.log(a); // undefined
 
 const obj = { a: 1 };
-console.log(obj.a);  // 1
-console.log(obj.b);  // undefined
+console.log(obj.a); // 1
+console.log(obj.b); // undefined
 
 const func = () => {};
 const c = func();
-console.log(c);  // undefined
+console.log(c); // undefined
+
+// null
+const n = null;
+console.log(typeof n); // object
+
+// 동등연산자(equality operator)
+console.log(n == undefined); // true
+console.log(n == null); // ture
+
+// 일치연산자(identity operator)
+console.log(n === undefined); // false
+console.log(n === null); // true
