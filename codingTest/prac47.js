81d498e74da675ca5feae1088d64f3b35b96128c
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-16 13:28:24 +0900

    chore: 배열의 원소중 정수만 더하기

--- codingTest/prac47.js
+++ codingTest/prac47.js
@@ -13,3 +13,10 @@ absolutes	signs	result
 [4,7,12]	[true,false,true]	9
 [1,2,3]	[false,false,true]	0
 */
+function solution(absolutes, signs) {
+  let answer = 0;
+  for (let i = 0; i < absolutes.length; i++) {
+    signs[i] ? (answer += absolutes[i]) : (answer -= absolutes[i]);
+  }
+  return answer;
+}
